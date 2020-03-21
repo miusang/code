c6fcb059dfe06385e72819eb2551fab172716ac7
atom <2223951512@qq.com>
2020-03-22 00:25:09 +0800

    pipe实现

--- IPC/pipe/pipe_demo.c
+++ IPC/pipe/pipe_demo.c
@@ -4,6 +4,11 @@
  * 1. 半双工，数据只能向一个方向流动，需要双方通信时，需要建立两个管道。
  * 2. 只能用于父子进程或兄弟进程之间（具有亲缘关系的进程）。
  * 3. 单独构成一种独立的文件系统，并且只存在于内存中。
+ *
+ * pipe(fd[2])函数:
+ *  成功返回0, fd数组创建2个文件描述符,fd[0]指向管道的读端,fd[1]指向管道的写端.
+ *  失败返回-1.
+ *
  */
 #include <stdio.h>
 #include <unistd.h>
@@ -11,15 +16,47 @@
 #include <string.h>
 
 int main(int argc, char** argv) {
-    int fd[4];
+    int fd[2];
     int res = pipe(fd);
     if (res < 0) {
-        printf("create pipe filed<%s>!\n", strerror(errno));
+        printf("create pipe error<%s>!\n", strerror(errno));
         return -1;
     }
 
-    for (int i = 0; i < 4; i++) {
-        printf("%d\n", fd[i]);
+    pid_t child1 = fork();
+    pid_t child2 = -1;
+    if (child1 != 0) {
+        child2 = fork();
     }
+
+    if (child1 == 0) {
+        int count = 0;
+        char buf[255];
+        while(1) {
+            memset(buf, 0, 10);
+            sprintf(buf, "child1 write data %d", count);
+            printf("pid %d %s\n", getpid(), buf);
+            write(fd[1], buf, 255);
+            count++;
+            usleep(1000*1000);
+        }
+    }
+
+    if (child2 == 0) {
+        char buf[255];
+        while(1) {
+            read(fd[0], buf, 255);
+            printf("pid %d child2 read data %s\n", getpid(), buf);
+            usleep(100*1000);
+        }
+    }
+
+    if (child1 != 0 && child2 != 0) {
+        while(1) {
+            printf("father %d waiting stop ...\n", getpid());
+            usleep(1000*1000);
+        }
+    }
+
     return 0;
 }
